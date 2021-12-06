7b67868b3dc521b55934e709ba40393699c02471
lubart2517 <lubomirvihvatniuk@gmail.com>
2021-12-07 00:28:01 +0200

    fix_api_init

--- library/rest/__init__.py
+++ library/rest/__init__.py
@@ -1,40 +1,13 @@
 """
-This package contains modules defining department and employee REST APIs and
-functions to initialize respective API endpoints:
+This package contains modules defining book and author REST APIs a
 Modules:
-- `department_api.py`: defines model representing departments
-- `employee_api.py`: defines model representing employees
-Functions:
-- `init_api`: register REST API endpoints
+- `books_api.py`: defines model representing books
+- `author_api.py`: defines model representing authors
+
 """
 
 from . import books_api
 from . import authors_api
 
 
-def init_api():
-    """
-    Register REST Api endpoints
-    :return: None
-    """
-    api.add_resource(
-        books_api.BookListApi,
-        '/api/books',
-        strict_slashes=False
-    )
-    api.add_resource(
-        books_api.BookApi,
-        '/api/book/<uuid>',
-        strict_slashes=False
-    )
-    api.add_resource(
-        authors_api.AuthorListApi,
-        '/api/authors',
-        strict_slashes=False
-    )
-    api.add_resource(
-        authors_api.AuthorApi,
-        '/api/author/<uuid>',
-        strict_slashes=False
-    )
 
