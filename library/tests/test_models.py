6de0cbceb1b0394ae3b470162bba0272c30c776d
lubart2517 <lubomirvihvatniuk@gmail.com>
2021-12-06 22:25:13 +0200

    try_test_without_fix

--- library/tests/test_models.py
+++ library/tests/test_models.py
@@ -1,11 +1,12 @@
 import os,sys,inspect
 
 # move to two level above for correct load library app
+'''
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 parentdir2 = os.path.dirname(parentdir)
 sys.path.insert(0,parentdir2)
-
+'''
 from library.models.author_models import Author
 from library.models.book_models import Book
 from library.models.order_models import Order
