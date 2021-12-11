43f4150631ddd56d9730db4c585663414181c00c
lubart2517 <lubomirvihvatniuk@gmail.com>
2021-12-11 17:36:01 +0200

    lol

--- library/__init__.py
+++ library/__init__.py
@@ -63,15 +63,15 @@ def create_app(config_class=DevelopmentConfig):
     )
 
     @app.errorhandler(403)
-    def forbidden():
+    def forbidden(error):
         return render_template('errors/403.html', title='Forbidden'), 403
 
     @app.errorhandler(404)
-    def page_not_found():
+    def page_not_found(error):
         return render_template('errors/404.html', title='Page Not Found'), 404
 
     @app.errorhandler(500)
-    def internal_server_error():
+    def internal_server_error(error):
         return render_template('errors/500.html', title='Server Error'), 500
     return app
 
