745d80d92faf8d7f18273f8255bf144e9e03f89e
lubart2517 <lubomirvihvatniuk@gmail.com>
2021-12-11 17:27:10 +0200

    fixed_init

--- library/__init__.py
+++ library/__init__.py
@@ -1,47 +1,43 @@
 # pylint: disable=wrong-import-position, protected-access, cyclic-import
-from flask import Flask, render_template, abort
+"""This module initialize Flask app"""
 import os
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import DevelopmentConfig
 from flask_bootstrap import Bootstrap
 from flask_restful import Api
+from config import DevelopmentConfig
+from .models.user_models import User
+from .views.blueprint import home as home_blueprint
+from.views.blueprint import auth as auth_blueprint, user as user_blueprint, admin as admin_blueprint
+
 MIGRATION_DIR = os.path.join('library', 'migrations')
 
 db = SQLAlchemy()
 
 
 def create_app(config_class=DevelopmentConfig):
+    """Creates main flask application"""
     app = Flask(__name__, template_folder="templates")
     app.config.from_object(config_class)
-
     Bootstrap(app)
     db.init_app(app)
     api = Api(app)
-    migrate = Migrate(app, db, directory=MIGRATION_DIR)
+    # migrate = Migrate(app, db, directory=MIGRATION_DIR)
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
-
-    from .models.user_models import User
-    from .views.blueprint import home as home_blueprint
-
     app.register_blueprint(home_blueprint)
-
-    from.views.blueprint import auth as auth_blueprint, user as user_blueprint, admin as admin_blueprint
-
     app.register_blueprint(auth_blueprint)
-
     app.register_blueprint(user_blueprint)
-
     app.register_blueprint(admin_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.get(user_id)
-    from library import models, forms
-    from .models import author_models, book_models, user_models, order_models
+    # from library import models, forms
+    # from .models import author_models, book_models, user_models, order_models
 
     from .rest import books_api, authors_api
     api.add_resource(
@@ -66,15 +62,15 @@ def create_app(config_class=DevelopmentConfig):
     )
 
     @app.errorhandler(403)
-    def forbidden(error):
+    def forbidden():
         return render_template('errors/403.html', title='Forbidden'), 403
 
     @app.errorhandler(404)
-    def page_not_found(error):
+    def page_not_found():
         return render_template('errors/404.html', title='Page Not Found'), 404
 
     @app.errorhandler(500)
-    def internal_server_error(error):
+    def internal_server_error():
         return render_template('errors/500.html', title='Server Error'), 500
     return app
 
